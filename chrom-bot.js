3c675345d980d95ced0b129a8e9d23d3b6f2a5e7
assassane <assahimmi@gmail.com>
2018-03-16 22:29:48 +0000

    Update chrom-bot.js

--- chrom-bot.js
+++ chrom-bot.js
@@ -1,16 +1,55 @@
-//Mzk3ODU3MTE2MjM4NjQzMjAw.DWb37w.IlFwKc5lh818A5cP3MhYPpDHn7E
-const Discord = require('discord.js');
-const bot = new Discord.Client();
+Bonjour a toutes et a tous, aujourd’hui je vais vous presenter mon projet qui s’intitule le realisme dans l’oeuvre.
+La presentation se deroulera en trois parties :
+d’abord nous allons parler du realisme et de ses specifité et aussi des autres courants litteraires en lien avec lui.
+Et ensuite, nous apliquerons cela a bel ami.
 
-bot.on('ready', function () {
-  console.log("Je suis connecté !");
-});
+1-le realisme :
 
-bot.on('message', message => {
-  if (message.content === 'ping') {
-    message.reply('pong !');
-  }
-});
+le courant realiste est un courant litteraire apparut vers 1850, il cherche a decrire la realité de la maniera la plus fidel qui soit et ceux sans la modifier ou l’enjoliver et donner une image exacte du reel comme un temoin qui rapporterait ce qu’il voit. Un ecriveain realiste se doit de decrire la realité comme le ferais un peintre a travers ses dessins, il est ainsi possible d’aborder des thèmes comme le travail salarié, les relations conjugales, ou les affrontements sociaux.
 
-bot.login('Mzk3ODU3MTE2MjM4NjQzMjAw.DWb37w.IlFwKc5lh818A5cP3MhYPpDHn7E');
+Dans la preface de pierre et jean, Maupassant disait :
 
+« Le romancier, au contraire, qui prétend nous donner une image exacte de la vie, doit éviter avec soin tout enchaînement d’événements qui paraîtrait exceptionnel. Son but n’est point de nous raconter une histoire, de nous amuser ou de nous attendrir, mais de nous forcer à penser, à comprendre le sens profond et caché des événements. À force d’avoir vu et médité il regarde l’univers, les choses, les faits et les hommes d’une certaine façon qui lui est propre et qui résulte de l’ensemble de ses observations réfléchies. C’est cette vision personnelle du monde qu’il cherche à nous communiquer en la reproduisant dans un livre. Pour nous émouvoir, comme il l’a été lui-même par le spectacle de la vie, il doit la reproduire devant nos yeux avec une scrupuleuse ressemblance. […]
+
+Le réaliste, s’il est un artiste, cherchera, non pas à nous montrer la photographie banale de la vie, mais à nous en donner la vision plus complète, plus saisissante, plus probante que la réalité même. »
+
+oui, car le realiste est avant tout un artiste, il ne cherche pas a faire un simple compte rednu de la realité, il doit mettre donc une certaine subjectivité.
+
+Le realisme est venu pour s’opposer au courant romantique qui lui privilegiait notamment l’expression du moi et les themes de la nature et de l’amour.
+
+Duranty disait a ce sujet :
+« Beaucoup de romanciers, non réalistes, ont la manie de faire exclusivement dans leurs œuvres l'histoire des âmes et non celle des hommes tout entiers. […] Or, au contraire, la société apparaît avec de grandes divisions ou professions qui font l'homme et lui donnent une physionomie plus saillante encore que celle qui lui est faite par ses instincts naturels; les principales passions de l'homme s'attachent à sa profession sociale, elle exerce une pression sur ses idées, ses désirs, son but, ses actions »
+
+le naturalisme est lui un prolongement du courant realiste mais en ajoutant un contexte physiologique et en montrant que le milieu où vit le protagoniste est l'une des raisons de son comportement. Il  a ete cree essentiellement par Zola ou il essaye d’appliquer la methode scientifique au recit, il considere que le romancier est un observateur et un experimentateur :
+« l'observateur et l'expérimentateur sont les seuls qui travaillent à la puissance et au bonheur de l'homme, en le rendant peu à peu le maître de la nature. Il n'y a ni noblesse, ni dignité, ni beauté, ni moralité, à ne pas savoir, à mentir, à prétendre qu'on est d'autant plus grand qu'on se hausse davantage dans l'erreur et dans la confusion. Les seules œuvres grandes et morales sont les œuvres de vérité » Zola
+autre citation de zola qui definit le naturalisme:
+« J'en suis donc parvenu à ce point : le roman expérimental est une conséquence de l'évolution scientifique du siècle ; il continue et complète la physiologie, qui elle-même s'appuie sur la chimie et la physique ; il substitue à l'étude de l'homme abstrait, de l'homme métaphysique, l'étude de l'homme naturel, soumis aux lois physico-chimiques et déterminé par les influences du milieu ; il est en un mot la littérature de notre âge scientifique, comme la littérature classique et romantique a correspondu à un âge de scolastique et de théologie »
+
+
+
+ 
+
+
+
+
+
+
+
+2-l’oeuvre :
+
+pensez vous que bel-ami est une œuvre realiste ou naturaliste ?
+Non naturaliste car :
+- aucune prétention scientifique,
+- aucune influence héréditaire,
+- aucun déterminisme social prépondérant (le roman est justement l'histoire d'une ascension sociale réussie).
+Realiste car :
+C'est un roman réaliste par sa désillusion, son pessimisme et son cynisme(attitude face a la vie, maque de foi ou d’espoir en l’humanité).
+
+En s’attardant sur l’incipit de bel ami, on decouvre que le cadre spatio-temporelle est directement precisé : on devine que l’action se passe a paris, grace a des notations de l’ecrivain , comme par exemple, la rue Notre–Dame-de-Lorette,
+ou l’avenue du bois de boulogne et surtout les folies-bergère signe d’un debauche parisienne a l’epoque. En ce qui concerne le temps, la seul precision est que nous somme le 28 juin car Maupassant en tant qu’ecrivain realiste, decrit la societé de son temps et donc on devine aussi que cela se produit au 19eme siecle.
+Autre procedé, l’entrée du personnage se fait « in medias res » c'est-à-dire que le personnage est prit à un moment précis de sa vie.
+
+On rearque aussi l’importance accordé au personnage principal sa description physique, vêtement, autres, Le soin en ce qui concerne l'argent, la monnaie rendue par la marchande…
+car il ne faut pas oublier que le but de Maupassant est de decrire la realité tel qu’elle est, par son œuvre, il decrit tout les milieux sociaux, c’est un portraitiste cruel etun observateur pessimiste d’une societé corompu.
+
+Merci d’avoir suivi ma presentation. Avez vous des questions ?
